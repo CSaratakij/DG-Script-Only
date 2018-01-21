939fec36fb0da9f212a25e6cd1ea38795bb1ef11
Chatchai Saratakij <CSaratakij@gmail.com>
2018-01-21 20:35:46 +0700

    Make verify duplicate save id in editor script "SaveSetting".

--- Assets/Editor/SaveSetting.cs
+++ Assets/Editor/SaveSetting.cs
@@ -56,6 +56,11 @@ namespace DG.Editor
                 _ReportGenerateResult();
             }
 
+            if (GUILayout.Button("Check Duplicate ID")) {
+                _UpdateAllSaveAbleObject();
+                _CheckDuplicateIDPerKey();
+            }
+
             if (SaveInstance.IsSaveFileExists()) {
                 if (GUILayout.Button("Delete SaveFile")) {
                     _DeleteSaveGame();
@@ -120,7 +125,7 @@ namespace DG.Editor
         void _ReportGenerateResult()
         {
             var message = string.Format("Re-Generate all Save ID in this scene..\n Total Key : {0}\n Total Save Instance : {1}\n", objKeyList.Count, allObj.Length);
-            EditorUtility.DisplayDialog("Success", message, "OK");
+            EditorUtility.DisplayDialog("Generate ID : Success", message, "OK");
         }
 
         void _DeleteSaveGame(bool forceDelete = false)
@@ -129,11 +134,69 @@ namespace DG.Editor
                 var isConfirm = EditorUtility.DisplayDialog("Delete SaveGame", "Are you sure to delete a save file?", "Delete", "Cancel");
                 if (isConfirm) {
                     SaveInstance.DeleteSave();
-                    EditorUtility.DisplayDialog("Success", "SaveGame has been deleted..", "OK");
+                    EditorUtility.DisplayDialog("Generate ID : Success", "SaveGame has been deleted..", "OK");
                 }
             }
             else {
                 SaveInstance.DeleteSave();
+            }
+        }
+
+        void _CheckDuplicateIDPerKey()
+        {
+            var dulplicateKeys = new List<string>();
+            var dulplicateID = new List<uint>();
+
+            foreach (var pair in objDict) {
+
+                for (int i = 0; i < objDict[pair.Key].Count; i++) {
+
+                    var testID = objDict[pair.Key][i].ID;
+                    var total = 0;
+
+                    for (int j = 0; j < objDict[pair.Key].Count; j++) {
+
+                        if (objDict[pair.Key][j].ID == testID) {
+                            total += 1;
+                        }
+
+                        if (total > 1) {
+
+                            var key = pair.Key;
+                            var id = objDict[key][j].ID;
+
+                            if (!dulplicateKeys.Contains(key)) {
+                                dulplicateKeys.Add(key);
+                            }
+
+                            dulplicateID.Add(id);
+
+                            break;
+                        }
+                    }
+                }
+            }
+
+            if (dulplicateKeys.Count > 0) {
+
+                var logConflictKey = string.Format("\tKey : {0}", dulplicateKeys.Count);
+                var logConflictID = string.Format("\tSave Instance : {0}", dulplicateID.Count);
+
+                EditorUtility.DisplayDialog("Verify ID : Error",
+                            "[ Duplicate ID ] :\n" +
+                            "Please generate new ID...\n" + 
+                            "\n" +
+                            "Total conflicts : \n" +
+                            logConflictKey +
+                            "\n" +
+                            logConflictID,
+                        "OK");
+            }
+            else {
+                EditorUtility.DisplayDialog("Verify ID : Success",
+                        "No conflicts Key and ID.\n" +
+                        "No need to generate new ID.",
+                        "OK");
             }
         }
     }

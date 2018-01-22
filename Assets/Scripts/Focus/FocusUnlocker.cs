fd816c5d2a45244bf4adf53d907b6450a93e2712
Chatchai Saratakij <CSaratakij@gmail.com>
2018-01-23 04:36:19 +0700

    Added Focus unlocker.

--- Assets/Scripts/Focus/FocusUnlocker.cs
+++ Assets/Scripts/Focus/FocusUnlocker.cs
@@ -1,10 +1,131 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+
+#if UNITY_EDITOR
+    using UnityEditor;
+#endif
 
 namespace DG
 {
     public class FocusUnlocker : MonoBehaviour
     {
+        public enum UnlockType
+        {
+            Focus,
+            EditMode,
+            MoveMode
+        }
+
+        [SerializeField]
+        UnlockType[] unlockList;
+
+        [SerializeField]
+        Vector2 size;
+
+        [SerializeField]
+        LayerMask layerMask;
+
+
+        public bool IsUsed { get { return isUsed; } set { isUsed = value; } }
+
+
+        bool isUsed;
+
+        Collider2D hit;
+        GameObject target;
+
+
+#if UNITY_EDITOR
+        void OnDrawGizmosSelected() {
+            Gizmos.color = Color.magenta;
+            Gizmos.DrawWireCube(transform.position, size);
+
+            Handles.Label(transform.position, "Trigger Area");
+
+            if (unlockList.Length > 0) {
+                Handles.Label(transform.position + Vector3.down * 0.2f, "Unlock : \n" + _GetAllUnlockName());
+            }
+            else {
+                Handles.Label(transform.position + Vector3.down * 0.2f, "None 'Focus' ability to unlock.");
+            }
+        }
+#endif
+
+        void Update()
+        {
+            if (hit && !isUsed) {
+                _InputHandler();
+            }
+        }
+
+        void FixedUpdate()
+        {
+            hit = Physics2D.OverlapBox(transform.position, size, 0.0f, layerMask);
+        }
+
+        void _InputHandler()
+        {
+            if (Input.GetButtonDown("Interact")) {
+                _UnlockFocus();
+            }
+        }
+
+        void _UnlockFocus()
+        {
+            var worldWrappingControl = hit.gameObject.GetComponent<WorldWrappingController>();
+
+            if (worldWrappingControl) {
+                foreach (var unlockType in unlockList) {
+                    switch (unlockType) {
+                        case UnlockType.Focus:
+                            worldWrappingControl.IsCanFocus = true;
+                            break;
+
+                        case UnlockType.MoveMode:
+                            worldWrappingControl.IsCanMoveMode = true;
+                            break;
+
+                        case UnlockType.EditMode:
+                            worldWrappingControl.IsCanEditMode = true;
+                            break;
+                    }
+                }
+
+                _Disabled();
+            }
+            else {
+                Debug.Log("Can't unlock focus ability..");
+            }
+        }
+
+        void _Disabled()
+        {
+            isUsed = false;
+            gameObject.SetActive(false);
+            //hide here..
+            //set active (false) ?
+            // and its save agent -> save its used state?
+        }
+
+        string _GetAllUnlockName()
+        {
+            var result = "";
+            foreach (var unlockType in unlockList) {
+                switch (unlockType) {
+                    case UnlockType.Focus:
+                        result += "\t[ Focus ]";
+                        break;
+
+                    case UnlockType.MoveMode:
+                        result += "\t[ MoveMode ]";
+                        break;
+
+                    case UnlockType.EditMode:
+                        result += "\t[ EditMode ]";
+                        break;
+                }
+                result += "\n";
+            }
+            return result;
+        }
     }
 }
---
+++ Assets/Scripts/Focus/FocusUnlockerSaveAgent.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DG
+{
+    public class FocusUnlockerSaveAgent : SaveInstance
+    {
+    }
+}

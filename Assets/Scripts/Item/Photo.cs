ff022f485aabb527ead33ded47fc8f3248397ced
Chatchai Saratakij <CSaratakij@gmail.com>
2018-03-02 18:46:43 +0700

    Make Photo collectable.

---
+++ Assets/Scripts/Item/Photo.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+#if UNITY_EDITOR
+    using UnityEditor;
+#endif
+
+namespace DG
+{
+    public class Photo : Item
+    {
+        [SerializeField]
+        uint photoID;
+
+
+        public uint ID { get { return photoID; } } 
+
+
+#if UNITY_EDITOR
+        void OnDrawGizmos()
+        {
+            var label = string.Format("ID : {0}", photoID);
+            Handles.color = Color.blue;
+            Handles.Label(transform.position, label);
+        }
+#endif
+
+        public override void Collect()
+        {
+            base.Collect();
+            //Todo
+            //Send Unlock info on runtime.
+        }
+    }
+}
---
+++ Assets/Scripts/Item/PhotoSaveAgent.cs
@@ -0,0 +1,17 @@
+﻿//If photo has picked -> unlock photo whitin particular id.
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhotoSaveAgent : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

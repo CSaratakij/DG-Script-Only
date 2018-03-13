d43280a3fb47ec4eece984a5290d0089e9582523
Chatchai Saratakij <CSaratakij@gmail.com>
2018-03-14 05:09:35 +0700

    Added all photo to Global Photo

---
+++ Assets/Scripts/UI/IntroStoryBoardUI.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace DG
+{
+    public class IntroStoryBoardUI : MonoBehaviour
+    {
+        [SerializeField]
+        float showDelay;
+
+        [SerializeField]
+        float hideDelay;
+
+        [SerializeField]
+        RectTransform[] masks;
+
+
+        void Start()
+        {
+            if (SaveInstance.IsSaveFileExists()) { 
+                gameObject.SetActive(false);
+                return; 
+            }
+
+            StartCoroutine(_ShowStoryBoard_Callback());
+        }
+
+        IEnumerator _ShowStoryBoard_Callback()
+        {
+            PlayerController.isInCinematic = true;
+
+            foreach (RectTransform rect in masks) {
+                yield return new WaitForSeconds(showDelay);
+                rect.gameObject.SetActive(false);
+            }
+
+            yield return new WaitForSeconds(hideDelay);
+
+            PlayerController.isInCinematic = false;
+            gameObject.SetActive(false);
+        }
+    }
+}
